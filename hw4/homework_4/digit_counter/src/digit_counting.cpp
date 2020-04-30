9e40c74e7de01ce9be2f472d12402422fc6e7193
Dat Nguyen <lonelyicevalley@icloud.com>
2020-04-30 16:09:21 +0700

    hw4 digit counter, create CMake project from scratch

---
+++ hw4/homework_4/digit_counter/src/digit_counting.cpp
@@ -0,0 +1,10 @@
+#include "./digit_counting.h"
+
+bool ReturnTrue() {
+	return true;
+}
+double Warp(double number, double factor) {
+	number += factor;
+	number -= factor;
+	return number;
+}
---
+++ hw4/homework_4/digit_counter/src/digit_counting.h
@@ -0,0 +1,5 @@
+#pragma once
+
+bool ReturnTrue();
+double Warp(double number, double factor);
+int CountSameSignificantDigits(double a, double b);
